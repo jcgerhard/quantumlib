c145f5830c079aaedb9943dbb3198ba90e3d1b26
jcgerhard <29855366+jcgerhard@users.noreply.github.com>
2023-02-14 16:16:22 +0100

    Add cloudflow.baseURL()

--- src/cloudflow/cloudflow.ts
+++ src/cloudflow/cloudflow.ts
@@ -1,3 +1,14 @@
 const helloCloudflow = 'Hello from Cloudflow!';
 
-export const cloudflow = { helloCloudflow };
+/**
+ * A function to get and/or create a valid Cloudflow base URL
+ * @param {string} [url] - The (optional) base URL of the Cloudflow server like "http://127.0.0.1"
+ * @param {number} [port] - The (optional) port that is used to access the Cloudflow server like "9090"
+ * @returns {string} The base URL as a string
+ */
+export function baseURL(url = 'http://127.0.0.1', port = 9090): string {
+    const baseURL: URL = new URL(`${url}:${port}`);
+    return baseURL.toString();
+}
+
+export const cloudflow = { helloCloudflow, baseURL };
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,9 @@
+/**
+ * @type {string}
+ * @const
+ */
 const defaultUrl = 'mongodb://127.0.0.1:27017';
+
 const defaultName = 'nucleus';
 
 export const database = { defaultName, defaultUrl };
