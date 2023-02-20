69776f2186f85b99db8f1a5185c7534fa8995adc
jcgerhard <29855366+jcgerhard@users.noreply.github.com>
2023-02-20 21:53:34 +0100

    Adding new README logo

--- src/cloudflow/cloudflow.ts
+++ src/cloudflow/cloudflow.ts
@@ -1,14 +1,22 @@
-const helloCloudflow = 'Hello from Cloudflow!';
+/**
+ * The type definition for a Cloudflow baseURL
+ */
+interface IBaseUrl {
+    url?: string;
+    port?: number;
+}
 
 /**
  * A function to get and/or create a valid Cloudflow base URL
- * @param {string} [url] - The (optional) base URL of the Cloudflow server like "http://127.0.0.1"
- * @param {number} [port] - The (optional) port that is used to access the Cloudflow server like "9090"
- * @returns {string} The base URL as a string
+ * @param {IBaseUrl} [baseUrl] An optional object to change host and/or port of the base URL
+ * - Example 1: { url: 'https://my.cloudflow.com' }
+ * - Example 2: { port: 9988 }
+ * - Example 3: { url: 'https://my.cloudflow.com', port: 9988 }
+ * @return {string} The base URL as a string
  */
-export function baseURL(url = 'http://127.0.0.1', port = 9090): string {
-    const baseURL: URL = new URL(`${url}:${port}`);
+export function baseURL(baseUrl: IBaseUrl = { url: 'http://127.0.0.1', port: 9090 }): string {
+    const baseURL: URL = new URL(`${baseUrl.url}:${baseUrl.port}`);
     return baseURL.toString();
 }
 
-export const cloudflow = { helloCloudflow, baseURL };
+export const cloudflow = { baseURL };
