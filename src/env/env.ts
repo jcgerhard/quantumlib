58441ecb878aaf94b7fa7dec6dd68512f8c88b07
jcgerhard <29855366+jcgerhard@users.noreply.github.com>
2023-03-01 23:28:52 +0100

    Squashed commit

--- src/env/env.ts
+++ src/env/env.ts
@@ -3,13 +3,15 @@ import { userInfo, platform } from 'os';
 const userName = userInfo().username;
 const userHome = userInfo().homedir;
 
-const osType = platform.toString();
+const osType = platform();
 
-export const applicationsFolder = osType === 'darwin' ? '/Applications' : 'C:\\Program Files';
+const applicationsFolder = osType === 'darwin' ? '/Applications' : 'C:\\Program Files';
+const applicationsFolderBrowser = osType === 'darwin' ? '/Applications' : '/C:/Program Files';
 
 export const env = {
     userName,
     userHome,
     osType,
     applicationsFolder,
+    applicationsFolderBrowser,
 };
